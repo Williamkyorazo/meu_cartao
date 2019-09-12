ab05d07943d0e0e2bd150c74199c475e5dbdf731
Williamkyorazo <willian.seu@gmail.com>
2019-09-12 17:34:58 -0300

    full App

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,79 @@
 import 'package:flutter/material.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
+      title: 'Mi Card',
       home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
         appBar: AppBar(
           backgroundColor: Colors.red.shade800,
-          title: Text('Meu Cartão'),
+          title: Text('Mi Card'),
           centerTitle: true,
         ),
         body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 60.0,
+              backgroundImage: AssetImage('images/perfildev.jpg'),
+            ),
+            Text(
+              "William S. S.",
+              style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "PROGRAMMING STUDENT",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  fontFamily: 'Source Sans Pro',
+                  letterSpacing: 2.5),
+            ),
+            SizedBox(
+              width: 170.0,
+              height: 20.0,
+              child: Divider(
+                color: Colors.red,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Card(
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.red,
+                      ),
+                      title: Text(
+                        "011 96094-6690",
+                        style: TextStyle(color: Colors.black, fontSize: 20.0),
+                      ))),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Card(
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.mail,
+                        color: Colors.red,
+                      ),
+                      title: Text(
+                        "wpaulista_sp@hotmail.com",
+                        style: TextStyle(color: Colors.black, fontSize: 18.0),
+                      ))),
+            ),
+          ],
+        )),
       ),
     );
   }
